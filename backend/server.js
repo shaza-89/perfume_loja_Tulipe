84a9597f3b895a5b68199f5080bd356179bed101
shaza alsa <dhaza.hg@gmail.com>
2024-09-22 22:10:36 -0300

    pegar Api do backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Perfumes from './data/Products.js'
 const port = 5000;
 
 const app = express();
@@ -7,8 +8,14 @@ app.get("/", (req, res) => {
     res.send("Api is running...");
 });
 
+app.get("/api/Perfumes", (req, res) => {
+    res.json(Perfumes);
+});
 
-   
+app.get("/api/Perfumes/:id", (req, res) => {
+    const Perfume = Perfumes.find((p) => p._id === req.params.id);
+    res.json(Perfume);
+});
 
 
 app.listen(port , () => console.log(`server running on port ${port}`))
