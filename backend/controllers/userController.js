0d32bbe611cbdbf1fe35d0291030ce72cc9484b6
shaza alsa <dhaza.hg@gmail.com>
2024-09-26 10:41:17 -0300

    add user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,4 +15,67 @@ const authUser = asyncHandler(async (req, res) => {
 const registerUser = asyncHandler(async (req, res) => {
         res.send("Register user");
 
+});
+    
+// @desc    logout user
+// @route   POST /api/users/logout
+// @access  private
+const logoutUser = asyncHandler(async (req, res) => {
+        res.send("logout user");
+
+});
+    
+// @desc    Get user profile
+// @route   Get /api/users/profile
+// @access  Public
+const getUserProfile = asyncHandler(async (req, res) => {
+        res.send("get User Profile");
+
+});
+    
+// @desc    update user profile
+// @route   PUT /api/users/profile
+// @access  private
+const updateUserProfile = asyncHandler(async (req, res) => {
+        res.send("update User Profile");
+});
+    
+// @desc    Get users
+// @route   Get /api/users
+// @access  private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+    res.send("get Users ");
+});
+
+// @desc    Get user by id
+// @route   Get /api/users/:id
+// @access  private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+    res.send("get Users By ID ");
+});
+    
+// @desc    Delete users
+// @route   DELETE /api/users/:id
+// @access  private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+    res.send("delete User ");
+});
+    
+// @desc    update user
+// @route   PUT /api/users/:id
+// @access  private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+    res.send("update User");
     });
+    
+export {
+ authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+    }
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 connectDB();   //  connect to MongoDB
 const app = express();
 
@@ -16,6 +17,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/Perfumes", productRoutes);
+app.use("/api/users", userRoutes);
+
 
 app.use(notFound);
 
