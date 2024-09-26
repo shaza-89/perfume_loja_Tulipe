8a95e60993c2fbd70ace9d47d3dd301f7e9282ea
shaza alsa <dhaza.hg@gmail.com>
2024-09-26 19:58:06 -0300

    add frontend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,39 +1,62 @@
-import React from 'react'
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from "react-router-bootstrap";
-import logo from "../assests/styles/logo.png"
+import React, { useEffect, useState } from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../assests/styles/logo.png';
 
 const Header = () => {
-  return (
-      <header>
-          <Navbar bg='dark' variant='dark' expand="md" collapseOnSelect>
-              <Container>
-                  <LinkContainer to="/">
-                  <Navbar.Brand>
-                     <img src={logo} alt='perfume natural'></img>
-                      </Navbar.Brand>
-                  </LinkContainer>
-                  <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                  <Navbar.Collapse id='basic-navbar-nav'>
-                      <Nav className='ms-auto'>
-                          <LinkContainer to='/cart'>
-                              <Nav.Link>
-                                  <FaShoppingCart /> Carrinho
-                                  </Nav.Link>
-                          </LinkContainer>
-                          <LinkContainer to='/login'>
-                              <Nav.Link >
-                                  <FaUser /> Entrar
-                              </Nav.Link>
-                              </LinkContainer>
-                      </Nav>
-                  </Navbar.Collapse>
-              </Container>
-          </Navbar>
-      
-    </header>
-  )
-}
+    const [userInfo, setUserInfo] = useState(null);
 
-export default Header
+    useEffect(() => {
+        // Get user info from localStorage
+        const storedUserInfo = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+        setUserInfo(storedUserInfo);
+    }, []);
+
+    const logoutHandler = () => {
+        // Remove user info from localStorage when logging out
+        localStorage.removeItem('userInfo');
+        setUserInfo(null); // Update state after logout
+    };
+
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>
+                            <img src={logo} alt='perfume natural'></img>
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <FaShoppingCart /> Carrinho
+                                </Nav.Link>
+                            </LinkContainer>
+
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Perfil</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Sair</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>
+                                        <FaUser /> Entrar
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
