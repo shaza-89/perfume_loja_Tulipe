805e935f075f058bd979eba5fb4c97f65e345d24
shaza alsa <dhaza.hg@gmail.com>
2024-09-26 17:52:13 -0300

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import { Container } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const App = () => {
   return (
@@ -13,6 +17,8 @@ const App = () => {
          </Container>
       </main>
       <Footer />
+       <ToastContainer />
+
     </>
   )
 }
