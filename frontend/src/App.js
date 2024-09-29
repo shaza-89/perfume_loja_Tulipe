6789435732bda1c761d23a36a3a7aa5e242d5cb8
JUSTICIER0 <julio503g@gmail.com>
2024-09-29 02:02:17 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ErrorBoundary from './components/ErrorBoundary'; // Componente para manejar errores
+import './App.css'; // Archivo CSS para estilos generales
 
 const App = () => {
   return (
-    <>
+    <ErrorBoundary>
       <Header />
       <main className='py-3'>
         <Container>
           <Outlet />
-         </Container>
+        </Container>
       </main>
       <Footer />
-    </>
-  )
-}
+    </ErrorBoundary>
+  );
+};
 
-export default App
+export default App;
+
