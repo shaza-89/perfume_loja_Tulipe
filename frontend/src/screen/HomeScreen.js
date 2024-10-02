a589013e753ecf77651717191cb31efabe5f7795
JUSTICIER0 <julio503g@gmail.com>
2024-10-02 19:50:50 -0300

    Update HomeScreen.js

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from "react-bootstrap";
 import Perfume from '../components/Perfume';
-import Perfumes from '../Products';
-// import Loader from '../components/Loader';
+import axios from 'axios';
 
 const HomeScreen = () => {
+    const [perfumes, setPerfumes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchPerfumes = async () => {
+            try {
+                const { data } = await axios.get('/api/perfumes');
+                setPerfumes(data);
+                setLoading(false);
+            } catch (error) {
+                setError('Erro ao carregar os perfumes');
+                setLoading(false);
+            }
+        };
+
+        fetchPerfumes();
+    }, []);
+
     return (
         <>
-            {/* <Loader /> */}
-            <h1>últimos produtos</h1>
-            <Row>
-                {Perfumes.map((perfume) => ( // Cambiado a "perfume"
-                    <Col key={perfume._id} sm={12} md={6} lg={4} xl={3}>
-                        <Perfume perfume={perfume} /> {/* Cambiado a "perfume" */}
-                    </Col>
-                ))}
-            </Row>
+            <h1>Últimos produtos</h1>
+            {loading ? (
+                <div>Carregando...</div>
+            ) : error ? (
+                <div>{error}</div>
+            ) : (
+                <Row>
+                    {perfumes.map((perfume) => (
+                        <Col key={perfume._id} sm={12} md={6} lg={4} xl={3}>
+                            <Perfume perfume={perfume} />
+                        </Col>
+                    ))}
+                </Row>
+            )}
         </>
     );
 };
