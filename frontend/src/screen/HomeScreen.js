d2e58ae3c2fb3548e823c2a81358fc185c0b80a7
JUSTICIER0 <julio503g@gmail.com>
2024-10-02 19:36:17 -0300

    Update HomeScreen.js

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import { Row, Col } from "react-bootstrap"
-import Perfume from '../components/Perfume'
-import Perfumes from '../Products'
-// import Loader from '../components/Loader'
+import React from 'react';
+import { Row, Col } from "react-bootstrap";
+import Perfume from '../components/Perfume';
+import Perfumes from '../Products';
+// import Loader from '../components/Loader';
 
 const HomeScreen = () => {
-  return (
-      <>
-          {/* <Loader /> */}
-          <h1>últimos produtos</h1>
-          <Row>
-              {Perfumes.map((Perfumes) => (
-                  <Col key={Perfumes._id} sm={12} md={6} lg={4} xl={3}>
-                      <Perfume Perfume={Perfumes} />
-                  </Col>
-              ))}
-          </Row>
-      
-    </>
-  )
-}
+    return (
+        <>
+            {/* <Loader /> */}
+            <h1>últimos produtos</h1>
+            <Row>
+                {Perfumes.map((perfume) => ( // Cambiado a "perfume"
+                    <Col key={perfume._id} sm={12} md={6} lg={4} xl={3}>
+                        <Perfume perfume={perfume} /> {/* Cambiado a "perfume" */}
+                    </Col>
+                ))}
+            </Row>
+        </>
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;
