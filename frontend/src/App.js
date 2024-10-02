1992ed2333ffe82e364fde145f8c077a04e9d5db
JUSTICIER0 <julio503g@gmail.com>
2024-10-02 19:54:00 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import ErrorBoundary from './components/ErrorBoundary'; // Componente para manejar errores
 import './App.css'; // Archivo CSS para estilos generales
+import axios from 'axios';
+
+// Puedes configurar axios para apuntar a tu backend aquí, si es necesario
+// axios.defaults.baseURL = 'http://localhost:5000'; // Ajusta la URL según tu configuración
 
 const App = () => {
   return (
@@ -21,4 +25,3 @@ const App = () => {
 };
 
 export default App;
-
